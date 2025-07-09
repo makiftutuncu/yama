1b93d6955239f4b103efe008fe28a686738a7e28
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2025-07-09 15:42:52 +0200

    Add more test

--- core/src/test/kotlin/dev/akif/yama/PatchDataTest.kt
+++ core/src/test/kotlin/dev/akif/yama/PatchDataTest.kt
@@ -1,5 +1,6 @@
 package dev.akif.yama
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
@@ -15,6 +16,16 @@ class PatchDataTest :
                         "email" to "test@test.com",
                         "address" to addressPatch
                     )
+            }
+
+            "throw exception when non-null property is given a null value" {
+                @Suppress("UNCHECKED_CAST")
+                val e =
+                    shouldThrow<CannotPatchToNullException> {
+                        val street = Present::class.constructors.first().call(null) as Omittable<String>
+                        PatchAddress(street = street, postalCode = Omitted, number = Omitted).toMap()
+                    }
+                e.name shouldBe "street"
             }
         }
     })
